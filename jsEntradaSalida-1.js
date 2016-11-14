81390d8b5e420d84b146c9c3e9386616f6f01865
Balush <manucamp_22@hotmail.com>
2016-11-14 11:39:56 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,11 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{
+//la ventana alert(), muestra mensaje emergente
+
+	alert("esto funciona perfeto");
+	
+	//literal, mensaje de texto
+	
+}
+
